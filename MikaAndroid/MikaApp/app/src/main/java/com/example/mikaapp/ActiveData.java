3b6d2feda5415723240dbf02a19b2f65d51f4714
Juan Manuel Martín Romero <juanma@ludiverso.com>
2024-05-26 11:50:19 +0200

    Ejemplo JavaDoc

--- MikaAndroid/MikaApp/app/src/main/java/com/example/mikaapp/ActiveData.java
+++ MikaAndroid/MikaApp/app/src/main/java/com/example/mikaapp/ActiveData.java
@@ -2,12 +2,29 @@ package com.example.mikaapp;
 
 import android.content.Context;
 
+/**
+ * Mantiene los datos de sesión en memoria
+ */
 public class ActiveData {
 
+    /**
+     * Datos del usuario activo en la aplicación
+     */
     static AuthenticateResponse loginData;
+    /**
+     * Datos de la ficha con la que se está trabajando
+     */
     static DatosFicha Ficha;
+    /**
+     * Identifica si es necesaria o no una sincronización con el servidor
+     */
     static boolean sincronizar;
 
+    /**
+     * Recibe los datos de autenticación del usuario y los guarda en la base de datos local si procede
+     * @param data datos de respuesta de la autenticación del usuario
+     * @param ctxt contexto del entorno de la aplicación
+     */
     public static void setLoginData(AuthenticateResponse data, Context ctxt){
         loginData = data;
         //si sincronizar viene a true es que hay que guardar en la tabla usuarios
